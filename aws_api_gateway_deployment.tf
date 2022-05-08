dcd1c1562ba17c4711fbe3316d56c5c93d9d8b6b
Gordon Murray <gordon@gordonmurray.com>
2022-05-08 22:31:18 +0100

    api improvements

--- aws_api_gateway_deployment.tf
+++ aws_api_gateway_deployment.tf
@@ -1,8 +1,8 @@
 
 resource "aws_api_gateway_deployment" "default" {
   rest_api_id       = aws_api_gateway_rest_api.default.id
-  stage_name        = "stage"
-  stage_description = "Staging stage"
+  stage_name        = "test"
+  stage_description = "Testing stage"
   depends_on        = [aws_api_gateway_integration.post]
   triggers = {
     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.default.body))
