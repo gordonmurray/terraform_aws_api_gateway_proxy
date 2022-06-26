0bfd1da403fe737f3c87e6525861cbe112b59880
Gordon Murray <gordon@gordonmurray.com>
2022-06-26 23:23:49 +0100

    remove unused files

--- aws_api_gateway_stage.tf
+++
@@ -1,7 +0,0 @@
-#resource "aws_api_gateway_stage" "default" {
-#  deployment_id         = aws_api_gateway_deployment.default.id
-#  rest_api_id           = aws_api_gateway_rest_api.default.id
-#  stage_name            = "test"
-#  xray_tracing_enabled  = "false"
-#  cache_cluster_enabled = "false"
-#}
