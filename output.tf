8c250681e07963009e3ba7538bc3223993331ff2
Gordon Murray <gordon@gordonmurray.com>
2022-06-27 21:32:12 +0100

    Show the URL to post to in the output

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "name" {
-  description = "API Gateway name"
-  value       = aws_api_gateway_rest_api.default.name
+output "post_data_to" {
+  description = "Deployment invoke url"
+  value       = "${aws_api_gateway_deployment.default.invoke_url}/${var.lambda_function_name}"
 }
