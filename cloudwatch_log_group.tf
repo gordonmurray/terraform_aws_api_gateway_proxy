6749119acf651dbf7252de407572c154e46d3384
Gordon Murray <gordon@gordonmurray.com>
2023-01-04 19:37:40 +0000

    corrected function name, add tags

--- cloudwatch_log_group.tf
+++ cloudwatch_log_group.tf
@@ -1,4 +1,8 @@
 resource "aws_cloudwatch_log_group" "lambda" {
-  name              = "/aws/lambda/${var.lambda_function_name}_lambda_logs"
+  name              = "/aws/lambda/${var.lambda_function_name}"
   retention_in_days = 1
+
+  tags = {
+    Name      = "/aws/lambda/${var.lambda_function_name}"
+  }
 }
