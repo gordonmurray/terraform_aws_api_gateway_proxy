ce638917c44d405ecfe25be2b1dc4f9d869ad0bf
Gordon Murray <gordon@gordonmurray.com>
2023-01-21 19:02:07 +0000

    Included a Checkly API check

--- main.tf
+++ main.tf
@@ -9,13 +9,24 @@ terraform {
       version = "4.20.0"
 
     }
+
+    checkly = {
+      source  = "checkly/checkly"
+      version = "1.4.3"
+    }
+
+
   }
 
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region                   = var.aws_region
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "gordonmurray"
+}
+
+provider "checkly" {
+  api_key    = var.checkly_api_key
+  account_id = var.checkly_account_id
 }
